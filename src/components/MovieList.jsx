b8e4f9cf327737791d826637f917d4cb052002a8
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-27 16:04:37 -0300

    Terminou o primeiro requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import SearchBar from './SearchBar';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    const {
+      movies,
+      searchText,
+      onSelectedGenreChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSearchTextChange,
+    } = this.props;
 
     return (
-      <div data-testid="movie-list" className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
+        <div data-testid="movie-list" className="movie-list">
+          { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        </div>
       </div>
     );
   }
@@ -19,6 +37,12 @@ MovieList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default MovieList;

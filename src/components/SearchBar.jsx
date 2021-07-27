68c83450772dad98610a00f18f13900e3daddb85
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-27 15:46:21 -0300

    Inserindo a class SerchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <p>teste</p>
+    );
+  }
+}
+
+export default SearchBar;

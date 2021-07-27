8489d7dd3853b0582d8d52d6b18b43a17843df94
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-27 15:40:50 -0300

    iniciando o projeto. Vamos Com Tudo :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,25 @@
-// implement MovieLibrary component here
+/* Para inicar o pull requesti copiado do https://github.com/tryber/sd-013-b-project-movie-cards-library-stateful
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary; */

93b87283941b5807fd1412fe38c60006e2c2d8ec
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-28 13:04:35 -0300

    Inserindo o componente Titulo

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Titulo extends React.Component {
+  render() {
+    const { title, handleChange } = this.props;
+    return (
+      <label htmlFor="titulo" data-testid="title-input-label">
+        Título
+        <input
+          type="text"
+          name="titulo"
+          id="titulo"
+          value={ title }
+          onChange={ handleChange }
+          data-testid="title-input"
+        />
+      </label>
+    );
+  }
+}
+
+Titulo.propTypes = {
+  title: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default Titulo;

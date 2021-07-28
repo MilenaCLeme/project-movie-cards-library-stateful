edfb0395c9b024c80af3044ee3563e8dbcbf4f25
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-28 13:06:59 -0300

    importando o data.js para o App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieLibrary from './components/MovieLibrary';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={ data } />
     </div>
   );
 }

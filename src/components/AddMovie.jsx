44b40104f48501c72b690582ad79c688daf37983
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-28 13:06:22 -0300

    Adicionando novo formulario para adicionar todo filme na lista - ainda não finalizado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,75 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Titulo from './Titulo';
+import Subtitulo from './Subtitulo';
+import Imagem from './Imagem';
+import Sinopse from './Sinopse';
+import Avaliacao from './Avaliacao';
+import Genero from './Genero';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <Titulo
+          value={ title }
+          handleChange={ this.handleChange }
+        />
+        <Subtitulo
+          value={ subtitle }
+          handleChange={ this.handleChange }
+        />
+        <Imagem
+          value={ imagePath }
+          handleChange={ this.handleChange }
+        />
+        <Sinopse
+          value={ storyline }
+          handleChange={ this.handleChange }
+        />
+        <Avaliacao
+          value={ rating }
+          handleChange={ this.handleChange }
+        />
+        <Genero
+          value={ genre }
+          handleChange={ this.handleChange }
+        />
+        <button onClick={ onClick } type="button" data-testid="send-button">
+          Adicionar Filme
+        </button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;

08393c8d53baff76ac8b0123c12241038d209e1a
MilenaCLeme <milenaleme4@hotmail.com>
2021-07-28 13:08:22 -0300

    Inserindo informações do movieLibrary para o movieList

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,25 +1,50 @@
-/* Para inicar o pull requesti copiado do https://github.com/tryber/sd-013-b-project-movie-cards-library-stateful
-import React, { Component } from 'react';
-
+import React from 'react';
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+class MovieLibrary extends React.Component {
   render() {
+    const {
+      movies,
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+      botao,
+    } = this.props;
     return (
       <div>
-        <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <h2>My awesome movie library</h2>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ onSearchTextChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ onBookmarkedChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ onSelectedGenreChange }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ botao } />
       </div>
     );
   }
 }
 
-export default MovieLibrary; */
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+  botao: PropTypes.func.isRequired,
+};
+
+export default MovieLibrary;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SearchBar from './SearchBar';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const {
-      movies,
-      searchText,
-      onSelectedGenreChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSearchTextChange,
-    } = this.props;
-
+    const { movies } = this.props;
     return (
       <div>
-        <SearchBar
-          searchText={ searchText }
-          onSearchTextChange={ onSearchTextChange }
-          bookmarkedOnly={ bookmarkedOnly }
-          onBookmarkedChange={ onBookmarkedChange }
-          selectedGenre={ selectedGenre }
-          onSelectedGenreChange={ onSelectedGenreChange }
-        />
         <div data-testid="movie-list" className="movie-list">
           { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
         </div>
@@ -37,12 +19,6 @@ MovieList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
-  searchText: PropTypes.string.isRequired,
-  onSearchTextChange: PropTypes.func.isRequired,
-  bookmarkedOnly: PropTypes.bool.isRequired,
-  onBookmarkedChange: PropTypes.func.isRequired,
-  selectedGenre: PropTypes.string.isRequired,
-  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default MovieList;

1c7d0c85fb71565b4f12b27a5f8f687f5187968d
MilenaCLeme <milenaleme4@hotmail.com>
2021-12-21 22:18:39 -0300

    motificando estilo

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BiSearchAlt2 } from 'react-icons/bi';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -114,23 +115,35 @@ class MovieLibrary extends React.Component {
       addMovie,
     } = this.state;
 
+    const iconeLupa = <BiSearchAlt2 />;
+
     return (
-      <div className="col align-self-center">
-        <h2>My awesome movie library</h2>
-        <button type="button" onClick={ this.onClickHide }>pesquisar</button>
-        <button type="button" onClick={ this.onClickAddMovie }>Adicionar Filme</button>
-        {
-          hide === false ? null : (
-            <SearchBar
-              searchText={ searchText }
-              onSearchTextChange={ this.onSearchTextChange }
-              bookmarkedOnly={ bookmarkedOnly }
-              onBookmarkedChange={ this.onBookmarkedChange }
-              selectedGenre={ selectedGenre }
-              onSelectedGenreChange={ this.onSelectedGenreChange }
-            />
-          )
-        }
+      <div>
+        <div className="sub-title-schear">
+          <h2>My awesome movie library</h2>
+          <button
+            type="submit"
+            onClick={ this.onClickHide }
+            className="distancia btn btn-dark"
+          >
+            { iconeLupa }
+          </button>
+        </div>
+        <div id="search-movie">
+          {
+            hide === false ? null : (
+              <SearchBar
+                searchText={ searchText }
+                onSearchTextChange={ this.onSearchTextChange }
+                bookmarkedOnly={ bookmarkedOnly }
+                onBookmarkedChange={ this.onBookmarkedChange }
+                selectedGenre={ selectedGenre }
+                onSelectedGenreChange={ this.onSelectedGenreChange }
+              />
+            )
+          }
+        </div>
+        <button type="button" onClick={ this.onClickAddMovie } className="bt-dispancia btn btn-outline-dark">Add Movie</button>
         {
           addMovie === false ? null : (
             <AddMovie onClick={ this.salvarMovies } />
